358363ad6085cb9336cc68b45a4b6a2440b0913b
ItoKobi <ito.kobilol@gmail.com>
2021-03-20 02:26:06 -0400

    commit

--- b-project/src/Pages/AnimePage.js
+++ b-project/src/Pages/AnimePage.js
@@ -4,37 +4,35 @@ import axios from 'axios';
 function AnimePage({match}) {
 
     const [data, setData] = useState({});
-    
+    const [loaded, setLoaded] = useState(false);
+
     useEffect(() => {
         async function request() {
             console.log("Start")
             await axios.get(`https://api.jikan.moe/v3/${match.params.type}/${match.params.mal_id}`)
             .then((res) => {
                 setData(res.data)
+                setLoaded(true);
             })
             .catch((err) => {
                 setData({})
             })}
             request();
-            console.log(data);
     }, [])
 
-    if(match.params.type === 'anime'){
+    if(match.params.type === 'anime' && loaded === true){
         return (
             <div>
-        <img src={data.image_url} alt={data.title} />
-        <h2>{data.title}</h2>
-        <h2>Episodes: {data.episodes}</h2>
-        <div>
-            <h4>Ending themes</h4>
-            { data && data.ending_themes ? <div>{data.ending_themes.forEach(element => {
-                <p>{element}</p>
-            })}</div> : <div>Not Loaded yet...</div>}
-        </div>
-    </div>
+                <img src={data.image_url} alt={data.title} />
+                <h2>{data.title} | {data.title_japanese} </h2>
+                <h2>Air: {data ? data.aired.string : <span>None</span> }</h2>
+                <h2>Episodes: {data.episodes}</h2>
+                <h2>Status: {data.status}</h2>
+                <p>{data.synopsis}</p>
+            </div>
     )}
     return(
-        <div>Not complete yet uwu...</div>
+        <div>Not loaded yet uwu...</div>
     )
 }
 
