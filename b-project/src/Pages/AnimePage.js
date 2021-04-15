fac670e17af3ff0397214aa062bf0ce058541a27
ItoKobi <ito.kobilol@gmail.com>
2021-04-15 10:48:03 -0400

    Minor fixes

--- b-project/src/Pages/AnimePage.js
+++ b-project/src/Pages/AnimePage.js
@@ -7,9 +7,8 @@ function AnimePage({match}) {
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
-        async function request() {
-            console.log("Start")
-            await axios.get(`https://api.jikan.moe/v3/${match.params.type}/${match.params.mal_id}`)
+        const request = () => {
+            axios.get(`https://api.jikan.moe/v3/${match.params.type}/${match.params.mal_id}`)
             .then((res) => {
                 setData(res.data)
                 setLoaded(true);
@@ -18,52 +17,51 @@ function AnimePage({match}) {
                 setData({})
             })}
             request();
-    }, [])
-    console.log(data)
+    },)
 
     if(match.params.type === 'anime' && loaded === true){
         return (
-            <div class="bg-gray-300 min-h-screen grid lg:grid-cols-2 p-8">
+            <div className="bg-gray-300 min-h-screen grid lg:grid-cols-2 p-8">
                 <div>
-                    <img src={data.image_url} alt={data.title} style={{width: "40%"}} class="block mx-auto py-2" />
-                    <iframe src={data.trailer_url} class="block mx-auto py-2" />
+                    <img src={data.image_url} alt={data.title} style={{width: "40%"}} className="block mx-auto py-2" />
+                    <iframe title={data.title} src={data.trailer_url} className="block mx-auto py-2" />
                 </div>
-                <div class="border border-gray-400 shadow-lg rounded p-2">
-                    <h2 class="font-bold text-4xl">{data.title} | {data.title_japanese} </h2>
-                    <h2><span class="font-bold">Air / Aired: </span>{data ? data.aired.string : <span>None</span> }</h2>
-                    <h2><span class="font-bold">Episode/s: </span>{data.episodes}</h2>
-                    <h2><span class="font-bold">Status: </span>{data.status}</h2>
-                    <p class="py-4">{data.synopsis}</p>
-                    <h2 class="font-bold">Genres:</h2>
+                <div className="border border-gray-400 shadow-lg rounded p-2">
+                    <h2 className="font-bold text-4xl">{data.title} | {data.title_japanese} </h2>
+                    <h2><span className="font-bold">Air / Aired: </span>{data ? data.aired.string : <span>None</span> }</h2>
+                    <h2><span className="font-bold">Episode/s: </span>{data.episodes}</h2>
+                    <h2><span className="font-bold">Status: </span>{data.status}</h2>
+                    <p className="py-4">{data.synopsis}</p>
+                    <h2 className="font-bold">Genres:</h2>
                     <h3>{data.genres.map(x => (
                         <div key={x.mal_id}>{x.name}</div>
                         ))}</h3>
-                    <h3><span class="font-bold">Rating: </span> {data.rating}</h3>
-                    <h3><span class="font-bold">Score: </span> {data.score}/10 out of {data.scored_by}</h3>
+                    <h3><span className="font-bold">Rating: </span> {data.rating}</h3>
+                    <h3><span className="font-bold">Score: </span> {data.score}/10 out of {data.scored_by}</h3>
                 </div>
             </div>
     )}else if(match.params.type === 'manga' && loaded === true){
         return (
-            <div class="bg-gray-300 min-h-screen grid grid-cols-2 p-8">
+            <div className="bg-gray-300 min-h-screen grid grid-cols-2 p-8">
                 <div>
-                    <img src={data.image_url} alt={data.title} style={{width: "40%"}} class="block mx-auto py-2" />
+                    <img src={data.image_url} alt={data.title} style={{width: "40%"}} className="block mx-auto py-2" />
                 </div>
-                <div class="border border-gray-400 shadow-lg rounded p-2">
-                    <h2 class="font-bold text-4xl">{data.title} | {data.title_japanese} </h2>
-                    <h2><span class="font-bold">Published: </span>{data ? data.published.string : <span>None</span> }</h2>
-                    <h2><span class="font-bold">Volume/s & Chapter/s: </span>{data.volumes}Vol. | {data.chapters}Chps.</h2>
-                    <h2><span class="font-bold">Status: </span>{data.status}</h2>
-                    <p class="py-4">{data.synopsis}</p>
-                    <h2 class="font-bold">Genres:</h2>
+                <div className="border border-gray-400 shadow-lg rounded p-2">
+                    <h2 className="font-bold text-4xl">{data.title} | {data.title_japanese} </h2>
+                    <h2><span className="font-bold">Published: </span>{data ? data.published.string : <span>None</span> }</h2>
+                    <h2><span className="font-bold">Volume/s & Chapter/s: </span>{data.volumes}Vol. | {data.chapters}Chps.</h2>
+                    <h2><span className="font-bold">Status: </span>{data.status}</h2>
+                    <p className="py-4">{data.synopsis}</p>
+                    <h2 className="font-bold">Genres:</h2>
                     <h3>{data.genres.map(x => (
                         <div key={x.mal_id}>{x.name}</div>
                         ))}</h3>                    
-                    <h3><span class="font-bold">Score: </span> {data.score}/10 out of {data.scored_by}</h3>
+                    <h3><span className="font-bold">Score: </span> {data.score}/10 out of {data.scored_by}</h3>
                 </div>
             </div>
     )}
     return(
-        <div class="text-center text-6xl font-bold">Not loaded yet uwu... or invalid parameters.</div>
+        <div className="text-center text-6xl font-bold">Not loaded yet uwu... or invalid parameters.</div>
     )
 }
 
--- b-project/src/Pages/MainPage.js
+++ b-project/src/Pages/MainPage.js
@@ -15,10 +15,9 @@ function MainPage() {
     }
 
     useEffect(() => {
-        let cancel = false;
-    async function request() {
-        if (input.length >= 3 || !cancel){
-        await axios.get(`https://api.jikan.moe/v3/search/${drop}?q=${input}`).then((res) => {
+     const request = () => {
+        if (input.length >= 3){
+        axios.get(`https://api.jikan.moe/v3/search/${drop}?q=${input}`).then((res) => {
             setSection(res.data.results)
         }).catch((err) => {
             setSection([])
@@ -26,32 +25,28 @@ function MainPage() {
             setSection([])
         }
     }
-        cancel = true;
-
-        console.log(section)
-
-        request();
+    request();
     }, [input, setInput, drop, setDrop])
     return (
-<div class="py-2 h-100 min-h-screen bg-gray-300 px-2">
-    <div class="max-w-md min-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden md:max-w-lg"></div>            
-        <div class="flex justify-center items-center">
-            <div class="max-w-md p-4">
-                <h1 class="text-center font-bold text-4xl py-2 ">Kobi Weeb Lookup</h1>
-                <div class="flex">
-                    <input onChange={changeInput} value={input} type="text" class="w-full h-12 rounded-l focus:outline-none px-3 focus:shadow-md" placeholder="Search..." /> 
-                    <select onChange={changeDrop} default={drop} class="rounded-r px-2 border-l-2 border-gray-100 outline-none">
+<div className="py-2 h-100 min-h-screen bg-gray-300 px-2">
+    <div className="max-w-md min-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden md:max-w-lg"></div>            
+        <div className="flex justify-center items-center">
+            <div className="max-w-md p-4">
+                <h1 className="text-center font-bold text-4xl py-2 ">Kobi Weeb Lookup</h1>
+                <div className="flex">
+                    <input onChange={changeInput} value={input} type="text" className="w-full h-12 rounded-l focus:outline-none px-3 focus:shadow-md" placeholder="Search..." /> 
+                    <select onChange={changeDrop} defaultValue={drop} className="rounded-r px-2 border-l-2 border-gray-100 outline-none">
+                        <option>Anime</option>
                         <option>Manga</option>
-                        <option selected="selected">Anime</option>
                     </select>
                 </div>
 
                 <ul>
             {section.map(data => (
                 <Link to={`/${drop}/${data.mal_id}`}>
-                    <li class="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
-                        <div class="flex ml-2"> <div class="rounded-full relative overflow-hidden w-12 h-12 border border-black"><img src={data.image_url} class="rounded-full" alt={data.title} /></div>
-                            <div class="flex flex-col ml-2"> <span class="font-medium truncate w-60 text-black">{data.title}</span> <span class="text-sm text-gray-400 truncate w-80">{data.synopsis}</span> </div>
+                    <li className="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
+                        <div className="flex ml-2"> <div className="rounded-full relative overflow-hidden w-12 h-12 border border-black"><img src={data.image_url} className="rounded-full" alt={data.title} /></div>
+                            <div className="flex flex-col ml-2"> <span className="font-medium truncate w-60 text-black">{data.title}</span> <span className="text-sm text-gray-400 truncate w-80">{data.synopsis}</span> </div>
                         </div>
                     </li>
                 </Link>
