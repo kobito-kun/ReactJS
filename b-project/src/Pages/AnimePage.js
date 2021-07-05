b822a615e61dfe04ef5d08b7cd52ffcd910d4e71
ItoKobi <ito.kobilol@gmail.com>
2021-07-05 00:03:18 -0400

    Updated

--- b-project/src/Pages/AnimePage.js
+++ b-project/src/Pages/AnimePage.js
@@ -1,67 +1,78 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import waifu from '../assets/waifu.png'
 
 function AnimePage({match}) {
 
     const [data, setData] = useState({});
     const [loaded, setLoaded] = useState(false);
 
+    const request = () => {
+        axios.get(`https://api.jikan.moe/v3/${match.params.type}/${match.params.mal_id}`)
+        .then((res) => {
+            setData(res.data)
+            setLoaded(true);
+        })
+        .catch((err) => {
+            setData({})
+    })}
+
     useEffect(() => {
-        const request = () => {
-            axios.get(`https://api.jikan.moe/v3/${match.params.type}/${match.params.mal_id}`)
-            .then((res) => {
-                setData(res.data)
-                setLoaded(true);
-            })
-            .catch((err) => {
-                setData({})
-            })}
-            request();
-    },)
+        console.log("fetching...")
+        request();
+        // eslint-disable-next-line
+    }, [])
 
     if(match.params.type === 'anime' && loaded === true){
         return (
-            <div className="bg-gray-300 min-h-screen grid lg:grid-cols-2 p-8">
-                <div>
-                    <img src={data.image_url} alt={data.title} style={{width: "40%"}} className="block mx-auto py-2" />
-                    <iframe title={data.title} src={data.trailer_url} className="block mx-auto py-2" />
+            <div className="bg-gray-300 min-h-screen pb-4">
+                <Link to="/" className="fixed top-2 uppercase bg-gray-600 px-4 py-2 text-white font-bold left-2">Return</Link>
+                <div className="p-4">
+                    <img src={data.image_url} alt={data.title} className="mx-auto h-96 shadow-lg" />
                 </div>
-                <div className="border border-gray-400 shadow-lg rounded p-2">
-                    <h2 className="font-bold text-4xl">{data.title} | {data.title_japanese} </h2>
+                <div className="border border-gray-400 shadow-lg rounded p-2 mx-4">
+                    <h2 className="font-thin text-4xl text-center">{data.title} | {data.title_japanese} </h2>
+                    <div className="flex justify-center flex-wrap my-4">
+                        {data.genres.map(x => ( <div className="rounded-full mx-1 shadow-lg bg-gray-600 inline-block px-2 text-white" key={x.mal_id}>{x.name}</div> ))}
+                    </div>
+                    <p className="mb-4">{data.synopsis}</p>
                     <h2><span className="font-bold">Air / Aired: </span>{data ? data.aired.string : <span>None</span> }</h2>
                     <h2><span className="font-bold">Episode/s: </span>{data.episodes}</h2>
                     <h2><span className="font-bold">Status: </span>{data.status}</h2>
-                    <p className="py-4">{data.synopsis}</p>
-                    <h2 className="font-bold">Genres:</h2>
-                    <h3>{data.genres.map(x => (
-                        <div key={x.mal_id}>{x.name}</div>
-                        ))}</h3>
                     <h3><span className="font-bold">Rating: </span> {data.rating}</h3>
                     <h3><span className="font-bold">Score: </span> {data.score}/10 out of {data.scored_by}</h3>
                 </div>
             </div>
     )}else if(match.params.type === 'manga' && loaded === true){
         return (
-            <div className="bg-gray-300 min-h-screen grid grid-cols-2 p-8">
-                <div>
-                    <img src={data.image_url} alt={data.title} style={{width: "40%"}} className="block mx-auto py-2" />
+            <div className="bg-gray-300 min-h-screen pb-4">
+                <Link to="/" className="fixed top-2 uppercase bg-gray-600 px-4 py-2 text-white font-bold left-2">Return</Link>
+                <div className="p-4">
+                    <img src={data.image_url} alt={data.title} className="mx-auto h-96 shadow-lg" />
                 </div>
                 <div className="border border-gray-400 shadow-lg rounded p-2">
-                    <h2 className="font-bold text-4xl">{data.title} | {data.title_japanese} </h2>
+                    <h2 className="font-thin text-4xl text-center">{data.title} | {data.title_japanese} </h2>
+                    <div className="flex justify-center flex-wrap my-4">
+                        {data.genres.map(x => ( <div key={x.mal_id} className="rounded-full mx-1 shadow-lg bg-gray-600 inline-block px-2 text-white">{x.name}</div> ))}
+                    </div>
+                    <p className="mb-4">{data.synopsis}</p>
                     <h2><span className="font-bold">Published: </span>{data ? data.published.string : <span>None</span> }</h2>
                     <h2><span className="font-bold">Volume/s & Chapter/s: </span>{data.volumes}Vol. | {data.chapters}Chps.</h2>
                     <h2><span className="font-bold">Status: </span>{data.status}</h2>
-                    <p className="py-4">{data.synopsis}</p>
-                    <h2 className="font-bold">Genres:</h2>
-                    <h3>{data.genres.map(x => (
-                        <div key={x.mal_id}>{x.name}</div>
-                        ))}</h3>                    
                     <h3><span className="font-bold">Score: </span> {data.score}/10 out of {data.scored_by}</h3>
                 </div>
             </div>
     )}
     return(
-        <div className="text-center text-6xl font-bold">Not loaded yet uwu... or invalid parameters.</div>
+        <>
+            <div className="h-screen bg-gray-300 text-white flex justify-center items-center text-center text-6xl font-bold">
+                <img src={waifu} alt="waifu" className="h-40" />
+                <span className="animate-pulse">
+                    Loading....
+                </span>
+            </div>
+        </>
     )
 }
 
--- b-project/src/Pages/MainPage.js
+++ b-project/src/Pages/MainPage.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import waifuOWO from '../assets/waifu.png';
 import {Link} from 'react-router-dom'
 import axios from 'axios';
 
@@ -27,34 +28,33 @@ function MainPage() {
     }
     request();
     }, [input, setInput, drop, setDrop])
-    return (
-<div className="py-2 h-100 min-h-screen bg-gray-300 px-2">
-    <div className="max-w-md min-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden md:max-w-lg"></div>            
-        <div className="flex justify-center items-center">
-            <div className="max-w-md p-4">
-                <h1 className="text-center font-bold text-4xl py-2 ">Kobi Weeb Lookup</h1>
-                <div className="flex">
-                    <input onChange={changeInput} value={input} type="text" className="w-full h-12 rounded-l focus:outline-none px-3 focus:shadow-md" placeholder="Search..." /> 
-                    <select onChange={changeDrop} defaultValue={drop} className="rounded-r px-2 border-l-2 border-gray-100 outline-none">
-                        <option>Anime</option>
-                        <option>Manga</option>
-                    </select>
-                </div>
 
-                <ul>
-            {section.map(data => (
-                <Link to={`/${drop}/${data.mal_id}`}>
-                    <li className="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
-                        <div className="flex ml-2"> <div className="rounded-full relative overflow-hidden w-12 h-12 border border-black"><img src={data.image_url} className="rounded-full" alt={data.title} /></div>
-                            <div className="flex flex-col ml-2"> <span className="font-medium truncate w-60 text-black">{data.title}</span> <span className="text-sm text-gray-400 truncate w-80">{data.synopsis}</span> </div>
-                        </div>
-                    </li>
-                </Link>
-            ))}
-                </ul>
+    return (
+        <div className="min-h-screen bg-gray-300">
+            <div className={`flex ${ input.length === 0 ? "h-screen" : "" } justify-center items-center`}>
+                <div className="p-4">
+                    <img src={waifuOWO} alt="Waifu" className="h-40 mx-auto select-none" />
+                    <div className="flex lg:px-0 px-2">
+                        <input onChange={changeInput} value={input} type="text" className="w-full h-12 rounded-l focus:outline-none px-3 focus:shadow-md" placeholder="Search..." /> 
+                        <select onChange={changeDrop} defaultValue={drop} className="rounded-r px-2 border-l-2 border-gray-100 outline-none">
+                            <option>Anime</option>
+                            <option>Manga</option>
+                        </select>
+                    </div>
+                    <ul>
+                    {section.map(data => (
+                        <Link key={data.mal_id} to={`/${drop}/${data.mal_id}`}>
+                            <li className="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
+                                <div className="flex ml-2"> <div className="rounded-full relative overflow-hidden w-12 h-12 border border-black"><img src={data.image_url} className="rounded-full" alt={data.title} /></div>
+                                    <div className="flex flex-col ml-2"> <span className="font-medium truncate w-60 text-black">{data.title}</span> <span className="text-sm text-gray-400 truncate w-60 lg:w-80">{data.synopsis}</span> </div>
+                                </div>
+                            </li>
+                        </Link>
+                    ))}
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
